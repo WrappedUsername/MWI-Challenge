057efa6d5b1b2d6d38259ccd9858411078c5bc87
Keefe Overby <you@example.com>
2022-07-18 23:58:40 -0500

    Work In Progress - mySQL

---
+++ mwi_challenge_arrays.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `array_1`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Matt Johnson` VARCHAR(255) NOT NULL,
+    `Matt Johnson` VARCHAR(255) NOT NULL,
+    `Bart Paden` VARCHAR(255) NOT NULL,
+    `Ryan Doss` VARCHAR(255) NOT NULL,
+    `Jared Malcolm` VARCHAR(255) NOT NULL
+);
+--@BLOCK
+CREATE TABLE `array_2`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Matt Johnson` VARCHAR(255) NOT NULL,
+    `Bart Paden` VARCHAR(255) NOT NULL,
+    `Bart Paden` VARCHAR(255) NOT NULL,
+    `Jordan Heigle` VARCHAR(255) NOT NULL,
+    `Jordan Heigle` VARCHAR(255) NOT NULL,
+    `Tyler Viles` VARCHAR(255) NOT NULL
+);
+--@BLOCK
+CREATE TABLE `results_array`(
+    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Matt Johnson` VARCHAR(255) NOT NULL,
+    `Bart Paden` VARCHAR(255) NOT NULL,
+    `Ryan Doss` VARCHAR(255) NOT NULL,
+    `Jared Malcolm` VARCHAR(255) NOT NULL,
+    `Jordan Heigle` VARCHAR(255) NOT NULL,
+    `Tyler Viles` VARCHAR(255) NOT NULL
+);
